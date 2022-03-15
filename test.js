eba8d0e3037e942e23b5ee4ec1c291cd18e223b2
Artemiy Schukin <shuckinart@yandex.ru>
2022-03-15 14:13:44 +0300

    feat: Decrease dev-dependencies size & more tests

--- test.js
+++ test.js
@@ -1,6 +1,36 @@
-const test = require("ava")
+const { test } = require("uvu")
+const { is } = require("uvu/assert")
 const driveURL = require(".")
 
-test("main", (t) => {
-	t.is(driveURL("https://drive.google.com/file/d/1Px8bePd7pFSz5r6bTA7GKN9HloCzMfFk/view?usp=sharing"), "https://drive.google.com/uc?export=download&id=1Px8bePd7pFSz5r6bTA7GKN9HloCzMfFk")
+test("main", () => {
+	is(driveURL("https://drive.google.com/file/d/1Px8bePd7pFSz5r6bTA7GKN9HloCzMfFk/view?usp=sharing"), "https://drive.google.com/uc?export=download&id=1Px8bePd7pFSz5r6bTA7GKN9HloCzMfFk")
+	is(driveURL("https://drive.google.com/file/d/1Px8bePd7pFSz5r6bTA7GKN9HloCzMfFk/view?usp=sharing", "supersecretkey"), "https://www.googleapis.com/drive/v3/files/1Px8bePd7pFSz5r6bTA7GKN9HloCzMfFk?alt=media&key=supersecretkey")
 })
+
+test("errors", () => {
+	try {
+		driveURL(null)
+	} catch (error) {
+		is(error.message, "Invalid URL provided.")
+	}
+
+	try {
+		driveURL("")
+	} catch (error) {
+		is(error.message, "Invalid URL provided.")
+	}
+
+	try {
+		driveURL("url", "")
+	} catch (error) {
+		is(error.message, "Invalid api key provided.")
+	}
+
+	try {
+		driveURL("url", "$$$")
+	} catch (error) {
+		is(error.message, "Invalid api key provided.")
+	}
+})
+
+test.run()
